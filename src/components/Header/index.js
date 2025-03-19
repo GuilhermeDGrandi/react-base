cd9f6a4729c87c4d7246fc05f69b6fc86f0dc4de
guilherme <guilhermedgrandi@gmail.com>
2025-03-19 18:59:47 -0300

    fazendo componente myroute para validar posteriormente se o usuario está logado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ export default function Header(){
         <Nav>
             <Link to="/"><FaHome size={24} /></Link>
             <Link to="/login"><FaUserAlt size={24} /></Link>
-            <Link to="/qualquercoisa"><FaSignInAlt size={24} /></Link>
+            <Link to="/teste"><FaSignInAlt size={24} /></Link>
         </Nav>
 
     )
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-import {Switch, Route} from 'react-router-dom'
+import {Switch} from 'react-router-dom'
 
+import MyRoute from "./MyRoute";
 import Login from '../pages/Login'
 import Page404 from '../pages/Page404'
 
@@ -9,12 +10,10 @@ export default function Routes(){
     return(
         
             <Switch>
-                <Route exact path='/' component={Login} />
-                <Route exact path='*' component={Page404} />
-            </Switch>    
+                <MyRoute exact path='/' component={Login} />
+                <MyRoute path='*' component={Page404} />
+            </Switch>          
         
-        
-
     )
 }
 
